1bc0c3fd23e32ceffc906e9f7a9d65628c0a2ad0
Steven Spencer <steve35spencer@gmail.com>
2020-07-26 15:37:07 -0400

    smarter bad guys chase you and bounce off each other. still cant hurt you. currently buggy as all hell

--- assets/badguy.js
+++ assets/badguy.js
@@ -1,15 +1,17 @@
 export default class BadGuy{
 
-    constructor(gameWidth, gameHeight, badguyImg, initXpos, initYpos){
+    constructor(name, gameWidth, gameHeight, badguyImg, initXpos, initYpos){
         this.badguyImg = badguyImg;
+        this.name = name
 
         this.gameWidth = gameWidth;
         this.gameHeight = gameHeight;
         this.width = 25;
         this.height = 25;
+        this.counter = 0;
 
-        this.speedX = 6.5;
-        this.speedY = 6.5;
+        this.speedX = 4;
+        this.speedY = 4;
 
         this.position = {
             x: initXpos,
@@ -20,10 +22,42 @@ export default class BadGuy{
     draw(ctx) {
         ctx.drawImage(this.badguyImg, this.position.x, this.position.y, this.width, this.height)
     }
+    
+    chasePlayer(player){
+        if(this.position.x > player.x){
+            this.position.x -= this.speedX;
+        }
+        if(this.position.x < player.x){
+            this.position.x += this.speedX;
+        }
+        if(this.position.y > player.y){
+            this.position.y -= this.speedY;
+        }
+        if(this.position.y < player.y){
+            this.position.y += this.speedY;
+        }
+        if(this.position.x < player.x + player.width && this.position.x + this.width > player.x && this.position.y < player.y + player.height && this.position.y + this.height > player.y){
+            
+        }
+    }
 
-    update(deltaTime) {
-        this.position.x += this.speedX;
-        this.position.y += this.speedY;
+    collisionDetection(others){
+        for(let i = 0; i < others.length; i++){
+            if(others[i].name != this.name){
+                if(this.position.x < others[i].position.x + others[i].width && this.position.x + this.width > others[i].position.x && this.position.y < others[i].position.y + others[i].height && this.position.y + this.height > others[i].position.y){
+                    others[i].speedY = -others[i].speedY
+                    others[i].speedX = -others[i].speedX
+                    this.speedY = -this.speedY
+                    this.speedX = -this.speedX
+                }
+            }
+        }
+    }
+
+    update(deltaTime, player, others) {
+        this.chasePlayer(player);
+        this.collisionDetection(others);
+        
 
         if (this.position.x < 0){
             this.position.x = 0;
--- index.js
+++ index.js
@@ -16,10 +16,11 @@ const HEIGHT = 600;
 ctx.clearRect(0,0, WIDTH, HEIGHT)
 
 let player = new Player(WIDTH, HEIGHT, playerImg);
-let badguy1 = new BadGuy(WIDTH, HEIGHT, badGuyImg, (Math.floor(Math.random() * 800) + 1), (Math.floor(Math.random() * 600) + 1));
-let badguy2 = new BadGuy(WIDTH, HEIGHT, badGuyImg, (Math.floor(Math.random() * 800) + 1), (Math.floor(Math.random() * 600) + 1));
-let badguy3 = new BadGuy(WIDTH, HEIGHT, badGuyImg, (Math.floor(Math.random() * 800) + 1), (Math.floor(Math.random() * 600) + 1));
+let badguy1 = new BadGuy("badguy1", WIDTH, HEIGHT, badGuyImg, (Math.floor(Math.random() * 800) + 1), (Math.floor(Math.random() * 600) + 1));
+let badguy2 = new BadGuy("badguy2",WIDTH, HEIGHT, badGuyImg, (Math.floor(Math.random() * 800) + 1), (Math.floor(Math.random() * 600) + 1));
+let badguy3 = new BadGuy("badguy3",WIDTH, HEIGHT, badGuyImg, (Math.floor(Math.random() * 800) + 1), (Math.floor(Math.random() * 600) + 1));
 
+const badGuys = [badguy1, badguy2, badguy3]
 player.draw(ctx);
 
 new InputHandler(player);
@@ -28,6 +29,12 @@ new InputHandler(player);
 let lastTime = 0;
 
 function gameLoop(timestamp) {
+    let playerLoc = {
+        x: player.position.x,
+        y: player.position.y,
+        width: player.width,
+        height: player.height
+    }
     let deltaTime = timestamp - lastTime
     lastTime = timestamp
 
@@ -35,11 +42,11 @@ function gameLoop(timestamp) {
     player.update(deltaTime);
     player.draw(ctx);
 
-    badguy1.update(deltaTime);
+    badguy1.update(deltaTime, playerLoc, badGuys);
     badguy1.draw(ctx);
-    badguy2.update(deltaTime);
+    badguy2.update(deltaTime, playerLoc, badGuys);
     badguy2.draw(ctx);
-    badguy3.update(deltaTime);
+    badguy3.update(deltaTime, playerLoc, badGuys);
     badguy3.draw(ctx);
 
     requestAnimationFrame(gameLoop)
